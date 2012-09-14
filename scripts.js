e4efc956375de9df8205b82bd685b43aad4935c9
Jacob O'Donnell <jacobodonnell@gmail.com>
2012-09-13 23:43:07 -0400

    a little refactoring

--- scripts.js
+++ scripts.js
@@ -1,14 +1,20 @@
-var time_wasting_websites = [/reddit.com/, /facebook.com/]
-
 $(document).ready(function(){
+    block_websites();
+    eztv();
+});
+
+function eztv() {
+    if (window.location.href.match(/http:\/\/eztv.it\/showlist\//)) {
+        $('.forum_thread_post').find(':contains(Airing)').parent().parent().css('background-color', '#3B3');
+    }
+}
+
+function block_websites() {
+    var time_wasting_websites = [/reddit.com/, /facebook.com/];
+
     for (var i = 0; i < time_wasting_websites.length; i++) {
         if (window.location.host.match(time_wasting_websites[i])) {
 	    $('body').html('You do not like this website.');
         }
     }
-
-    if (window.location.href.match(/http:\/\/eztv.it\/showlist\//)) {
-        $('.forum_thread_post').find(':contains(Airing)').parent().parent().css('background-color', '#3B3');
-    }
-
-});
+}
